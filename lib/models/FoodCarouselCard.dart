0c235945abe6a6b8a723278250a262ab1d0b29ca
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-07-21 14:22:57 +0100

    Update

--- lib/models/FoodCarouselCard.dart
+++ lib/models/FoodCarouselCard.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
 class FoodCard {
   String headingImage;
-  String color;
+  Color color;
   String title;
   String image;
+  FoodCard({this.color, this.headingImage, this.image, this.title});
 }
