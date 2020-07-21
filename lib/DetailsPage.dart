d43a6bcf9f4fab457993ddfd4082ab9afbe84edf
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-07-21 12:55:21 +0100

    New Screen

---
+++ lib/DetailsPage.dart
@@ -0,0 +1,42 @@
+import "package:flutter/material.dart";
+
+class Details extends StatefulWidget {
+  @override
+  _DetailsState createState() => _DetailsState();
+}
+
+class _DetailsState extends State<Details> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 200,
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.search), onPressed: () {})
+              ],
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Padding(
+                  padding: EdgeInsets.only(top: 50),
+                  child: CircleAvatar(
+                    radius: 50,
+                  ),
+                ),
+              ),
+              backgroundColor: Colors.red,
+              leading: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
