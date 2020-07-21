8171373e992038f0d2fe9d2186b584893c1be6f1
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-07-21 14:23:03 +0100

    Update

---
+++ lib/providers/FoodCarouselProvider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:order_food/models/FoodCarouselCard.dart';
+import 'package:order_food/providers/BaseProvider.dart';
+
+class CardProvider extends BaseProvider {
+  List<FoodCard> get items {
+    return <FoodCard>[
+      FoodCard(
+          color: Colors.red,
+          title: 'KFC',
+          headingImage: 'images/kfc.png',
+          image: 'images/food.png'),
+    ];
+  }
+}
