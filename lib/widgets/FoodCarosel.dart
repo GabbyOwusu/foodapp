bd1316ecd677fb315136162628e1fd292e35dba3
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-07-22 15:29:46 +0100

    Update

--- lib/widgets/FoodCarosel.dart
+++ lib/widgets/FoodCarosel.dart
@@ -16,59 +16,62 @@ class FoodCarousel extends StatefulWidget {
 class _FoodCarouselState extends State<FoodCarousel> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      scrollDirection: Axis.vertical,
-      children: <Widget>[
-        SizedBox(height: 60),
-        AnimatedContainer(
-          duration: Duration(milliseconds: 300),
-          curve: Curves.easeInOutQuint,
-          height: widget.active ? 0 : 80,
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 30),
-          child: SizedBox(
-            height: 100,
-            child: Hero(
-              tag: widget.tag,
-              child: Image.asset(widget.foodCard.headingImage),
-            ),
+    return Container(
+      // color: widget.active ? widget.foodCard.color : null,
+      child: ListView(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          SizedBox(height: 60),
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            curve: Curves.easeInOutQuint,
+            height: widget.active ? 0 : 80,
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 10, right: 10, top: 30),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Details()));
-            },
-            child: Container(
-              padding: EdgeInsets.all(20),
-              height: 800,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20))),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    height: 250,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(widget.foodCard.image)),
-                      color: widget.foodCard.color,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  )
-                ],
+          Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: SizedBox(
+              height: 100,
+              child: Hero(
+                tag: widget.tag,
+                child: Image.asset(widget.foodCard.headingImage),
               ),
             ),
           ),
-        ),
-      ],
+          Padding(
+            padding: EdgeInsets.only(left: 10, right: 10, top: 30),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Details()));
+              },
+              child: Container(
+                padding: EdgeInsets.all(20),
+                height: 800,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20))),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 250,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(widget.foodCard.image)),
+                        color: widget.foodCard.color,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
