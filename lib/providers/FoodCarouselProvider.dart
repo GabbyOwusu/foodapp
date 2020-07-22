7a45d2a63ed37dbedefec1857d02b5a9ea6ccf8b
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-07-22 17:43:22 +0100

    Update

--- lib/providers/FoodCarouselProvider.dart
+++ lib/providers/FoodCarouselProvider.dart
@@ -7,24 +7,24 @@ class CardProvider extends BaseProvider {
     return <FoodCard>[
       FoodCard(
           color: Color.fromRGBO(251, 84, 71, 1),
-          title: 'McDonnalds',
+          title: 'McDonnald\'s',
           headingImage: 'images/macdonnalds.png',
           image: 'images/food.png'),
       FoodCard(
           color: Color.fromRGBO(0, 150, 127, 1),
-          title: 'KFC',
+          title: 'Starbucks',
           headingImage: 'images/starbucks.png',
           image: 'images/food.png'),
       FoodCard(
           color: Color.fromRGBO(26, 186, 24, 1),
-          title: 'KFC',
+          title: 'Subway',
           headingImage: 'images/subway.png',
           image: 'images/subwayfood.png'),
       FoodCard(
           color: Color.fromRGBO(255, 87, 96, 1),
           title: 'KFC',
           headingImage: 'images/kfc.png',
-          image: 'images/food.png'),
+          image: 'images/chickenbucket.png'),
     ];
   }
 }
