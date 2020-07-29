a44919b4594317e2160424e709035de7d1b593b9
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-07-29 14:08:03 +0100

    Update

--- lib/widgets/foodgrid.dart
+++ lib/widgets/foodgrid.dart
@@ -1,48 +1,34 @@
 import 'package:flutter/material.dart';
 
 class FoodGrid extends StatelessWidget {
-  const FoodGrid({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GridView.builder(
-        primary: false,
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          margin: EdgeInsets.only(left: 20),
-          width: 100,
-          height: 120,
-          decoration: BoxDecoration(
-              color: Color.fromRGBO(248, 242, 255, 1).withOpacity(0.7),
-              borderRadius: BorderRadius.circular(10)),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'images/chickenbucket.png',
-                  width: 50,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Breakfast',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontFamily: 'SanFransisco',
-                      letterSpacing: 1),
-                )
-              ],
+    return Container(
+      margin: EdgeInsets.only(left: 20),
+      width: 100,
+      height: 120,
+      decoration: BoxDecoration(
+          color: Color.fromRGBO(248, 242, 255, 1).withOpacity(0.7),
+          borderRadius: BorderRadius.circular(10)),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'images/chickenbucket.png',
+              width: 50,
             ),
-          ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Breakfast',
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'SanFransisco',
+                  letterSpacing: 1),
+            )
+          ],
         ),
       ),
     );
