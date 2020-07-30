702517e7c5cd60a30460719b433ba7cf0d832748
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-07-30 14:11:11 +0100

    Update

--- lib/widgets/FoodCarosel.dart
+++ lib/widgets/FoodCarosel.dart
@@ -32,9 +32,9 @@ class _FoodCarouselState extends State<FoodCarousel> {
         children: <Widget>[
           SizedBox(height: 60),
           AnimatedContainer(
-            duration: Duration(milliseconds: 100),
+            duration: Duration(milliseconds: 290),
             curve: Curves.easeInOutQuint,
-            height: widget.active ? 0 : 100,
+            height: widget.active ? 0 : 60,
           ),
           Padding(
             padding: EdgeInsets.only(top: 30),
