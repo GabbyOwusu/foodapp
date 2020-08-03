8c011f42d4dc4c834f35edff98506a0e1bacae70
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-03 11:30:53 +0100

    Update

--- lib/widgets/foodgrid.dart
+++ lib/widgets/foodgrid.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class FoodGrid extends StatefulWidget {
   final int index;
+
   FoodGrid({@required this.index});
 
   @override
@@ -10,61 +11,64 @@ class FoodGrid extends StatefulWidget {
 
 class _FoodGridState extends State<FoodGrid> {
   bool active = false;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: () {
-          setState(() {
-            active = !active;
-          });
-        },
-        child: Container(
-          margin: EdgeInsets.only(left: 20, right: 20),
-          width: 210,
-          height: 210,
-          decoration: BoxDecoration(
-              color: active
-                  ? Colors.red
-                  : Color.fromRGBO(248, 242, 255, 1).withOpacity(0.7),
-              borderRadius: BorderRadius.circular(10)),
-          child: Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Column(
-              children: <Widget>[
-                Center(
-                  child: Image.asset(
-                    'images/chickenbucket.png',
-                    width: 100,
+      onTap: () {
+        setState(() {
+          active = !active;
+        });
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 100),
+        margin: EdgeInsets.only(left: 20, right: 20),
+        width: 210,
+        height: 210,
+        decoration: BoxDecoration(
+            color: active
+                ? Colors.red
+                : Color.fromRGBO(248, 242, 255, 1).withOpacity(0.7),
+            borderRadius: BorderRadius.circular(10)),
+        child: Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: Image.asset(
+                  'images/chickenbucket.png',
+                  width: 100,
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, bottom: 10),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Big Breakfast meal',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'SanFransisco',
+                        color: active ? Colors.white : Colors.black,
+                        letterSpacing: 1),
                   ),
                 ),
-                Spacer(),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10, bottom: 10),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Big Breakfast meal',
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10, bottom: 20),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'AED',
                       style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontFamily: 'SanFransisco',
-                          color: active ? Colors.white : Colors.black,
-                          letterSpacing: 1),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 10, right: 10, bottom: 20),
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        'AED',
-                        style: TextStyle(
-                          color: active ? Colors.white : Colors.black,
-                        ),
+                        color: active ? Colors.white : Colors.black,
                       ),
-                      Spacer(),
-                      Container(
+                    ),
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
                         height: 20,
                         width: 20,
                         decoration: BoxDecoration(
@@ -77,13 +81,15 @@ class _FoodGridState extends State<FoodGrid> {
                           color: Colors.black26,
                           size: 15,
                         )),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
