7cccbb9762849656f7bb83ad326a4dcd817d7d96
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-05 08:26:59 +0100

    Update

--- lib/widgets/orderSheet.dart
+++ lib/widgets/orderSheet.dart
@@ -8,135 +8,128 @@ class Order extends StatefulWidget {
 class _OrderState extends State<Order> {
   @override
   Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-      initialChildSize: 0.8,
-      builder: (context, controller) => SingleChildScrollView(
-        controller: controller,
-        child: AnimatedContainer(
-          duration: Duration(
-            milliseconds: 400,
+    return AnimatedContainer(
+      duration: Duration(
+        milliseconds: 400,
+      ),
+      height: 680,
+      decoration: BoxDecoration(
+        color: Colors.white, //Colors.grey[100],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
           ),
-          height: 800,
-          decoration: BoxDecoration(
-            color: Colors.orange, //Colors.grey[100],
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
+          Container(
+            height: 6,
+            width: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.grey[300].withOpacity(0.9),
             ),
           ),
-          child: Column(
+          SizedBox(height: 20),
+          SizedBox(
+            height: 200,
+            width: 200,
+            child: Image.asset('images/burger.png'),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Big Mac Burger',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'SanFransisco',
+              height: 1.3,
+              fontSize: 25,
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            'AED 24',
+            style: TextStyle(
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'SanFransisco',
+              height: 1.3,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'A big and tasty Halal beef patty smothered in \nour one of a kind Big Tasty Sauce and 3\n slices of emmental cheese...',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.grey,
+              fontFamily: 'SanFransisco',
+              height: 1.3,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 150,
+            child: ListView.builder(
+              itemBuilder: (context, index) => snackSuggestions(),
+              itemCount: 4,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              SizedBox(
-                height: 20,
-              ),
               Container(
-                height: 6,
-                width: 60,
+                margin: EdgeInsets.only(left: 20, top: 10),
                 decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[400], width: 2),
                   borderRadius: BorderRadius.circular(30),
-                  color: Colors.grey[300].withOpacity(0.9),
                 ),
-              ),
-              SizedBox(height: 20),
-              SizedBox(
-                height: 200,
-                width: 200,
-                child: Image.asset('images/burger.png'),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Big Mac Burger',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'SanFransisco',
-                  height: 1.3,
-                  fontSize: 25,
+                child: Row(
+                  children: [
+                    IconButton(
+                        icon: (Icon(Icons.add)),
+                        color: Colors.grey[400],
+                        onPressed: () {}),
+                    Text('1'),
+                    IconButton(
+                        icon: (Icon(Icons.add)),
+                        color: Colors.grey[400],
+                        onPressed: () {}),
+                  ],
                 ),
-              ),
-              SizedBox(height: 5),
-              Text(
-                'AED 24',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'SanFransisco',
-                  height: 1.3,
-                  fontSize: 16,
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'A big and tasty Halal beef patty smothered in \nour one of a kind Big Tasty Sauce and 3\n slices of emmental cheese...',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontFamily: 'SanFransisco',
-                  height: 1.3,
-                  fontSize: 16,
-                ),
-              ),
-              SizedBox(
-                height: 20,
               ),
               Container(
-                height: 150,
-                child: ListView.builder(
-                  itemBuilder: (context, index) => snackSuggestions(),
-                  itemCount: 4,
-                  scrollDirection: Axis.horizontal,
+                margin: EdgeInsets.only(right: 20, top: 10),
+                width: 200,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(left: 20, top: 10),
-                    //width: 150,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey[400], width: 2),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Row(
-                      children: [
-                        IconButton(
-                            icon: (Icon(Icons.add)),
-                            color: Colors.grey[400],
-                            onPressed: () {}),
-                        Text('1'),
-                        IconButton(
-                            icon: (Icon(Icons.add)),
-                            color: Colors.grey[400],
-                            onPressed: () {}),
-                      ],
+                child: Center(
+                  child: Text(
+                    'Add to cart',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'SanFransisco',
+                      height: 1.3,
+                      fontSize: 20,
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.only(right: 20, top: 10),
-                    width: 200,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Add to cart',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'SanFransisco',
-                          height: 1.3,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
+                ),
               )
             ],
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
