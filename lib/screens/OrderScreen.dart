f9de56b2faed0022a99809b1eb65cdec31d8c0a8
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-05 12:47:02 +0100

    Update

--- lib/screens/OrderScreen.dart
+++ lib/screens/OrderScreen.dart
@@ -10,7 +10,7 @@ class _OderScreenState extends State<OderScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      //backgroundColor: Colors.grey[100].withOpacity(0.3),
       appBar: AppBar(
         backgroundColor: Colors.white,
         centerTitle: true,
@@ -24,7 +24,10 @@ class _OderScreenState extends State<OderScreen> {
         elevation: 0,
         title: Text(
           'MacDonalds',
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'SanFransisco',
+              fontWeight: FontWeight.bold),
         ),
       ),
       body: SingleChildScrollView(
@@ -51,10 +54,102 @@ class _OderScreenState extends State<OderScreen> {
                     fontWeight: FontWeight.w600,
                     fontFamily: 'SanFransisco'),
               ),
+              SingleChildScrollView(
+                primary: false,
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    item(
+                      title: 'Coca-Cola Zero',
+                      image: 'images/burger.png',
+                      text: 'AED 24',
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    item(
+                      title: 'Coca-Cola Zero',
+                      image: 'images/burger.png',
+                      text: 'AED 24',
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+Widget item({String title, String text, String image}) {
+  return Container(
+    padding: EdgeInsets.only(left: 10),
+    width: 270,
+    height: 100,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+    ),
+    margin: EdgeInsets.only(
+      top: 20,
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Image.asset(
+          image,
+          width: 50,
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontFamily: 'SanFransisco',
+                  fontSize: 17,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                text,
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Container(
+            height: 30,
+            width: 30,
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.transparent,
+              border: Border.all(
+                width: 2,
+                color: Color.fromRGBO(235, 235, 250, 1),
+              ),
+            ),
+            child: Center(
+                child: Icon(
+              Icons.add,
+              color: Colors.black26,
+              size: 15,
+            )),
+          ),
+        ),
+      ],
+    ),
+  );
 }
