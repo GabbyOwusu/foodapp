3dbb62ed6a48bdf88385fc7c4e430f63ad153844
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-06 16:46:42 +0100

    Update

--- lib/screens/DetailsPage.dart
+++ lib/screens/DetailsPage.dart
@@ -21,227 +21,233 @@ class _DetailsState extends State<Details> {
     return Provider.of<CardProvider>(context);
   }
 
+  bool active = false;
+
+  bool adToCart = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: widget.foodcard.color,
-      body: SafeArea(
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              expandedHeight: 250,
-              actions: <Widget>[
-                IconButton(icon: Icon(Icons.search), onPressed: () {}),
-              ],
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                title: Padding(
-                  padding: EdgeInsets.only(top: 80),
-                  child: Hero(
-                    tag: 'me',
-                    child: Image.asset(widget.foodcard.image, width: 200),
+        backgroundColor: widget.foodcard.color,
+        body: SafeArea(
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: 250,
+                actions: <Widget>[
+                  IconButton(icon: Icon(Icons.search), onPressed: () {}),
+                ],
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Padding(
+                    padding: EdgeInsets.only(top: 80),
+                    child: Hero(
+                      tag: 'me',
+                      child: Image.asset(widget.foodcard.image, width: 200),
+                    ),
                   ),
                 ),
+                backgroundColor: Colors.transparent,
+                leading: IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
               ),
-              backgroundColor: Colors.transparent,
-              leading: IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate([
-                Column(
-                  children: <Widget>[
-                    Container(
-                      height: 1800,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
+              SliverList(
+                delegate: SliverChildListDelegate([
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        height: 1800,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20),
+                          ),
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Text(
+                              widget.foodcard.title,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 26,
+                                  fontFamily: 'SanFransisco',
+                                  letterSpacing: 0.5),
+                            ),
+                            SizedBox(height: 30),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 20, right: 20),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Icon(Icons.star,
+                                      color: Colors.yellow, size: 18),
+                                  Text(
+                                    '4.8',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey.withOpacity(0.8),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Burgers American',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey.withOpacity(0.8),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Dollars',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey.withOpacity(0.8),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            Container(
+                              height: 30,
+                              width: 130,
+                              decoration: BoxDecoration(
+                                color: Color.fromRGBO(235, 235, 250, 1)
+                                    .withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  '10 - 15 min',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 50),
+                              child: Container(
+                                height: 120,
+                                child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: 10,
+                                  itemBuilder: (context, index) => SnackList(
+                                    index: index,
+                                    foodCardimage: widget.foodcard,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 20, top: 30),
+                                child: Text(
+                                  'Breakfast Value Meals',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 50),
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 10, right: 10),
+                                child: GridView.builder(
+                                    primary: false,
+                                    shrinkWrap: true,
+                                    physics: NeverScrollableScrollPhysics(),
+                                    gridDelegate:
+                                        SliverGridDelegateWithFixedCrossAxisCount(
+                                            crossAxisCount: 2,
+                                            mainAxisSpacing: 10,
+                                            crossAxisSpacing: 10),
+                                    itemCount: 10,
+                                    itemBuilder: (context, index) {
+                                      return FoodGrid(index: index);
+                                    }),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 30,
-                          ),
-                          Text(
-                            widget.foodcard.title,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 26,
-                                fontFamily: 'SanFransisco',
-                                letterSpacing: 0.5),
-                          ),
-                          SizedBox(height: 30),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20, right: 20),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Icon(Icons.star,
-                                    color: Colors.yellow, size: 18),
-                                Text(
-                                  '4.8',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.grey.withOpacity(0.8),
-                                  ),
-                                ),
-                                Text(
-                                  'Burgers American',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.grey.withOpacity(0.8),
-                                  ),
-                                ),
-                                Text(
-                                  'Dollars',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.grey.withOpacity(0.8),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 30),
-                          Container(
-                            height: 30,
-                            width: 130,
-                            decoration: BoxDecoration(
-                              color: Color.fromRGBO(235, 235, 250, 1)
-                                  .withOpacity(0.5),
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: Center(
-                              child: Text(
-                                '10 - 15 min',
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 50),
-                            child: Container(
-                              height: 120,
-                              child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: 10,
-                                itemBuilder: (context, index) => SnackList(
-                                  index: index,
-                                  foodCardimage: widget.foodcard,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 20, top: 30),
-                              child: Text(
-                                'Breakfast Value Meals',
-                                style: TextStyle(
-                                    fontSize: 17, fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 50),
-                          Expanded(
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 10, right: 10),
-                              child: GridView.builder(
-                                primary: false,
-                                shrinkWrap: true,
-                                physics: NeverScrollableScrollPhysics(),
-                                gridDelegate:
-                                    SliverGridDelegateWithFixedCrossAxisCount(
-                                        crossAxisCount: 2,
-                                        mainAxisSpacing: 10,
-                                        crossAxisSpacing: 10),
-                                itemCount: 10,
-                                itemBuilder: (context, index) => FoodGrid(
-                                  index: index,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ]),
-            ),
-          ],
-        ),
-      ),
-      bottomSheet: Container(
-        height: 80,
-        padding: EdgeInsets.all(10),
-        color: Colors.white,
-        child: Row(
-          children: [
-            Container(
-              height: 40,
-              width: 30,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(248, 242, 255, 1).withOpacity(1),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Center(
-                child: Text(
-                  '1',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            Text(
-              'AED24',
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-            ),
-            Spacer(),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => OderScreen(),
+                    ],
                   ),
-                );
-              },
-              child: Container(
-                width: 200,
-                height: 60,
+                ]),
+              ),
+            ],
+          ),
+        ),
+        bottomSheet: Container(
+          height: 80,
+          padding: EdgeInsets.all(10),
+          color: Colors.white,
+          child: Row(
+            children: [
+              Container(
+                height: 40,
+                width: 30,
                 decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(50),
+                  color: Color.fromRGBO(248, 242, 255, 1).withOpacity(1),
+                  borderRadius: BorderRadius.circular(5),
                 ),
                 child: Center(
                   child: Text(
-                    'View cart',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'SanFransisco',
-                      height: 1.3,
-                      fontSize: 20,
+                    '1',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                'AED24',
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+              ),
+              Spacer(),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => OderScreen(),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: 200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'View cart',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SanFransisco',
+                        height: 1.3,
+                        fontSize: 20,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+            ],
+          ),
+        ));
   }
 }
