1b376914617e5fd119c71a7d15a17c31711775b0
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-11 09:30:54 +0100

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:order_food/providers/CartProvider.dart';
 import 'package:order_food/screens/Home.dart';
 import 'package:order_food/providers/FoodCarouselProvider.dart';
 import 'package:provider/provider.dart';
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => CardProvider())],
+      providers: [
+        ChangeNotifierProvider<CardProvider>(create: (_) => CardProvider()),
+        ChangeNotifierProvider<CartProvider>(create: (_) => CartProvider()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Order Food',
