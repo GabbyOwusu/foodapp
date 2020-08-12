ce3a0624b067344446bd621307a558dce09f49f4
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-12 12:05:24 +0100

    Update

--- lib/providers/CartProvider.dart
+++ lib/providers/CartProvider.dart
@@ -3,21 +3,23 @@ import 'package:order_food/models/Order.dart';
 import 'package:order_food/providers/BaseProvider.dart';
 
 class CartProvider extends BaseProvider {
-  List<Order> orderList = <Order>[];
+  List<Order> _orderList = <Order>[];
+
+  List<Order> get orders => _orderList.reversed.toList();
 
   void addOrder(Order order) {
-    orderList.add(order);
+    _orderList.add(order);
     notifyListeners();
   }
 
   void removeOrder(Order order) {
-    orderList.removeWhere((o) => o == order);
+    _orderList.removeWhere((o) => o == order);
     notifyListeners();
   }
 
   void update(Order oldOrder, Order newOrder) {
-    final index = orderList.indexWhere((o) => o == oldOrder);
-    orderList[index] = newOrder;
+    final index = _orderList.indexWhere((o) => o == oldOrder);
+    _orderList[index] = newOrder;
     notifyListeners();
   }
 }
