18d15dbabc169068f1bcfa59cc9abdbaf4973f9c
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-13 19:36:53 +0100

    Update

--- lib/widgets/FoodCarosel.dart
+++ lib/widgets/FoodCarosel.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:order_food/providers/FoodCarouselProvider.dart';
 import 'package:order_food/screens/DetailsPage.dart';
-import 'package:order_food/models/FoodCarouselCard.dart';
+import 'package:order_food/models/FoodCarouselModel.dart';
 import 'package:provider/provider.dart';
 
 class FoodCarousel extends StatefulWidget {
