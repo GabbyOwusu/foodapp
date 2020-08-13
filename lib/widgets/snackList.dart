5733d814a13c04b2c29e6acc041301e98161299e
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-13 19:37:17 +0100

    Update

--- lib/widgets/snackList.dart
+++ lib/widgets/snackList.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:order_food/models/FoodCarouselCard.dart';
+import 'package:order_food/models/FoodCarouselModel.dart';
 import 'package:order_food/providers/FoodCarouselProvider.dart';
 import 'package:provider/provider.dart';
 
