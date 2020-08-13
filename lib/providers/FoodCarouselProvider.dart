2c6a14347ddae92eb23eacbfad405e5cfd327f8a
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-13 19:36:24 +0100

    Update

--- lib/providers/FoodCarouselProvider.dart
+++ lib/providers/FoodCarouselProvider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:order_food/models/FoodCarouselCard.dart';
+import 'package:order_food/models/FoodCarouselModel.dart';
 import 'package:order_food/providers/BaseProvider.dart';
 
 class CardProvider extends BaseProvider {
