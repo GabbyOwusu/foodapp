f2846474c3b4dc6602af7d77c78f7856e92ac1d9
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-09-01 13:45:00 +0100

    Update

--- lib/widgets/oderCard.dart
+++ lib/widgets/oderCard.dart
@@ -2,52 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:order_food/models/FoodCarouselModel.dart';
 import 'package:order_food/models/Order.dart';
 
-class OrderCard extends StatelessWidget {
+class OrderCard extends StatefulWidget {
   final FoodCard foodTitle;
   final Order order;
-  //final int index;
 
   final VoidCallback onDelete;
   OrderCard({
-    // this.index,
     @required this.foodTitle,
     @required this.order,
     @required this.onDelete,
   });
 
   @override
+  _OrderCardState createState() => _OrderCardState();
+}
+
+class _OrderCardState extends State<OrderCard> {
+  @override
   Widget build(BuildContext context) {
+    //  final provider = Provider.of<CartProvider>(context);
     return Container(
       height: 100,
-      child: Row(
-        children: [
-          Image.asset(order.food.image, width: 60),
-          SizedBox(width: 50),
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Big Mac Burger',
-                  style: TextStyle(fontFamily: 'SanFransisco', fontSize: 17),
-                ),
-                Text(
-                  'AED 24',
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
-                ),
-              ],
-            ),
-          ),
-          Spacer(),
-          IconButton(
-              icon: Icon(Icons.remove_circle_outline),
-              color: Colors.red,
-              onPressed: () {
-                onDelete();
-              })
-        ],
+      child: ListTile(
+        leading: Image.asset(widget.order.food.image, width: 60),
+        title: Text(
+          widget.order.food.title,
+          style: TextStyle(fontFamily: 'SanFransisco', fontSize: 17),
+        ),
+        subtitle: Text(
+          'AED 24',
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+        ),
+        trailing: IconButton(
+            icon: Icon(Icons.remove_circle_outline),
+            color: Colors.red,
+            onPressed: () {
+              widget.onDelete();
+            }),
       ),
     );
+
+    // Row(
+    //   children: [
+    //     Image.asset(order.food.menu[index], width: 60),
+    //     SizedBox(width: 50),
+    //     Container(
+    //       margin: EdgeInsets.only(top: 20),
+    //       child: Column(
+    //         crossAxisAlignment: CrossAxisAlignment.start,
+    //         children: [
+    //           Text(
+    //             order.food.foodName[index],
+    //             style: TextStyle(fontFamily: 'SanFransisco', fontSize: 17),
+    //           ),
+    //           Text(
+    //             'AED 24',
+    //             style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //     Spacer(),
+    //     IconButton(
+    //         icon: Icon(Icons.remove_circle_outline),
+    //         color: Colors.red,
+    //         onPressed: () {
+    //           onDelete();
+    //         })
+    //   ],
+    // ),
   }
 }
