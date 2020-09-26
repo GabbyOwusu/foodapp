8b47daa1ead93f46fdfa5f0bfa1573bd53ce7edd
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-09-26 13:43:03 +0100

    Update

--- lib/models/FoodCarouselModel.dart
+++ lib/models/FoodCarouselModel.dart
@@ -4,20 +4,22 @@ class FoodCard {
   String headingImage;
   int height;
   Color color;
-  String title, description;
-  String image;
-  List<String> snacks;
-  List<String> menu;
-  List<String> foodName;
+  String title = '';
+  String description = '';
+  String image = '';
+  List<String> snacks = [];
+  List<String> menu = [];
+  List<String> foodName = [];
 
-  FoodCard(
-      {this.color,
-      this.description,
-      this.foodName,
-      this.snacks,
-      this.headingImage,
-      this.image,
-      this.title,
-      this.height,
-      this.menu});
+  FoodCard({
+    this.color,
+    this.description,
+    this.foodName,
+    this.snacks,
+    this.headingImage,
+    this.image,
+    this.title,
+    this.height,
+    this.menu,
+  });
 }
