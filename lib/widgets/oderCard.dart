1aa725e8ee1045c76fc9b0c0ca20b2d02a8bca0e
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-09-26 13:43:33 +0100

    Update

--- lib/widgets/oderCard.dart
+++ lib/widgets/oderCard.dart
@@ -5,10 +5,12 @@ import 'package:order_food/models/Order.dart';
 class OrderCard extends StatefulWidget {
   final FoodCard foodTitle;
   final Order order;
+  final int index;
 
   final VoidCallback onDelete;
   OrderCard({
     @required this.foodTitle,
+    @required this.index,
     @required this.order,
     @required this.onDelete,
   });
@@ -20,14 +22,17 @@ class OrderCard extends StatefulWidget {
 class _OrderCardState extends State<OrderCard> {
   @override
   Widget build(BuildContext context) {
-    ///  final provider = Provider.of<CartProvider>(context);
     return Container(
       height: 100,
       child: ListTile(
-        leading: Image.asset(widget.order.food.image, width: 60),
+        leading: Image.asset(widget.order.food.menu[widget.index], width: 60),
         title: Text(
-          widget.order.food.title,
-          style: TextStyle(fontFamily: 'SanFransisco', fontSize: 17),
+          widget.order.food.foodName[widget.index],
+          style: TextStyle(
+            fontFamily: 'SanFransisco',
+            fontSize: 17,
+            fontWeight: FontWeight.w600,
+          ),
         ),
         subtitle: Text(
           'AED 24',
@@ -40,35 +45,5 @@ class _OrderCardState extends State<OrderCard> {
         ),
       ),
     );
-
-    // Row(
-    //   children: [
-    //     Image.asset(order.food.menu[index], width: 60),
-    //     SizedBox(width: 50),
-    //     Container(
-    //       margin: EdgeInsets.only(top: 20),
-    //       child: Column(
-    //         crossAxisAlignment: CrossAxisAlignment.start,
-    //         children: [
-    //           Text(
-    //             order.food.foodName[index],
-    //             style: TextStyle(fontFamily: 'SanFransisco', fontSize: 17),
-    //           ),
-    //           Text(
-    //             'AED 24',
-    //             style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //     Spacer(),
-    //     IconButton(
-    //         icon: Icon(Icons.remove_circle_outline),
-    //         color: Colors.red,
-    //         onPressed: () {
-    //           onDelete();
-    //         })
-    //   ],
-    // ),
   }
 }
